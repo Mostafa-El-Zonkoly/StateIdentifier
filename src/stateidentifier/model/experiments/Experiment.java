6184145e7534c09a1aa571f95b5eb85f17753420
unknown <Zonkoly@Zonkoly-PC.(none)>
2012-03-09 18:18:02 +0200

    change

--- src/stateidentifier/model/experiments/Experiment.java
+++ src/stateidentifier/model/experiments/Experiment.java
@@ -1,9 +1,19 @@
 package stateidentifier.model.experiments;
 
+import stateidentifier.model.nodes.Node;
+
 public abstract class Experiment {
  // change
 	
 	// another channge
 	
 	// another damn change
+	
+	public void bfs(){
+		
+		//TODO: aa
+		
+		hasStoppingCondition(null);
+	}
+	public abstract boolean hasStoppingCondition(Node node);
 }
--- src/stateidentifier/model/experiments/HomingExperiment.java
+++ src/stateidentifier/model/experiments/HomingExperiment.java
@@ -1,5 +1,13 @@
 package stateidentifier.model.experiments;
 
+import stateidentifier.model.nodes.Node;
+
 public class HomingExperiment extends Experiment{
+
+	@Override
+	public boolean hasStoppingCondition(Node node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
